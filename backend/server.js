df669cdef6e654b6c1a6ecbd4dafb6399f4914af
yasamnoya <yasamnono@gmail.com>
2021-03-16 00:43:24 +0800

    setup mongodb

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 const app = express();
 
 app.use('/', (req, res) =>{
