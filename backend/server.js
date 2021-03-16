84a86aa5e28b5679240a9569103baa7be7a9c376
yasamnoya <yasamnono@gmail.com>
2021-03-16 13:16:31 +0800

    setup tests

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,12 @@
 require('dotenv').config()
+const app = require("./app")
 
-const express = require('express');
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
-const app = express();
 
-app.use(express.json());
-
-const declarationsRouter = require('./routes/declarations')
-app.use('/declarations', declarationsRouter);
 
 app.listen(8000, () => {console.log('Listening on port 8000')});
---
+++ backend/tests/declarations.test.js
@@ -0,0 +1,54 @@
+const request = require('supertest');
+const app = require('../app');
+const { setupDB } = require('./testSetup')
+
+const TEST_PATH = '/declarations'
+
+describe('Test CRUD to declarations', () =>{
+	setupDB('testDeclarations');
+
+	test('It should response the GET method', async () => {
+		const response = await request(app).get(TEST_PATH);
+		expect(response.statusCode).toBe(200);
+	});
+
+	test('It should response the POST method', async () => {
+		const body = {
+			name: "Jane Doe",
+			phone: "0900000000"
+		};
+		const response = await request(app).post(TEST_PATH).send(body);
+		expect(response.statusCode).toBe(201);
+		expect(response.body.name).toBe(body.name);
+		expect(response.body.phone).toBe(body.phone);
+	});
+
+	test('It should response the PATCH method', async () => {
+		const body = {
+			name: "Jane Doe",
+			phone: "0900000000"
+		};
+		const response = await request(app).post(TEST_PATH).send(body);
+
+		const newBody = {
+			name: "Jane Doe",
+			phone: "0900000000"
+		};
+		const newResponse = await request(app).patch(`${TEST_PATH}/${response.body.id}`).send(newBody);
+		expect(newResponse.statusCode).toBe(200)
+		expect(newResponse.body.name).toBe(newBody.name);
+		expect(newResponse.body.phone).toBe(newBody.phone);
+	});
+
+	test('It should response the DELETE method', async () => {
+		const body = {
+			name: "Jane Doe",
+			phone: "0900000000"
+		};
+		const response = await request(app).post(TEST_PATH).send(body);
+
+		const newResponse = await request(app).delete(`${TEST_PATH}/${response.body.id}`)
+		expect(newResponse.statusCode).toBe(200)
+	});
+});
+
